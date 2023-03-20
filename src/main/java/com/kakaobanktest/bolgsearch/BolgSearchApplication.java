9deec91272cdaf6a4a0ffe758549b1c43b9947a1
ebong22 <lhlbl@naver.com>
2023-03-21 08:22:51 +0900

    Initial commit

---
+++ src/main/java/com/kakaobanktest/bolgsearch/BolgSearchApplication.java
@@ -0,0 +1,13 @@
+package com.kakaobanktest.bolgsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolgSearchApplication.class, args);
+	}
+
+}
